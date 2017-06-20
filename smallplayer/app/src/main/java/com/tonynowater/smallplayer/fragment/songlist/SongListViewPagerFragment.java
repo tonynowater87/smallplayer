bded196d13a4ee786130171599434d0366ca7018
tonynowater87 <tonynowater87@gmail.com>
2017-06-21 00:09:26 +0800

    本地音樂畫面加上重讀本地音樂的Menu按鈕

--- smallplayer/app/src/main/java/com/tonynowater/smallplayer/fragment/songlist/SongListViewPagerFragment.java
+++ smallplayer/app/src/main/java/com/tonynowater/smallplayer/fragment/songlist/SongListViewPagerFragment.java
@@ -2,13 +2,19 @@ package com.tonynowater.smallplayer.fragment.songlist;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.tonynowater.smallplayer.MyApplication;
 import com.tonynowater.smallplayer.R;
 import com.tonynowater.smallplayer.base.BaseViewPagerFragment;
 import com.tonynowater.smallplayer.databinding.LayoutSonglistfragmentBinding;
 import com.tonynowater.smallplayer.fragment.u2bsearch.RecyclerViewDivideLineDecorator;
+import com.tonynowater.smallplayer.util.MediaUtils;
 import com.tonynowater.smallplayer.util.OnClickSomething;
 
 /**
@@ -16,6 +22,8 @@ import com.tonynowater.smallplayer.util.OnClickSomething;
  */
 public class SongListViewPagerFragment extends BaseViewPagerFragment<LayoutSonglistfragmentBinding> {
     private static final String TAG = SongListViewPagerFragment.class.getSimpleName();
+    private static final int MENU_ID_REFRESH = 111;
+    private static final int MENU_ORDER_NUMBER = 300;//和menu_main裡的orderInCategory相比，越大越靠右邊。
     private SongListAdapter mSongListAdapter;
 
     public static SongListViewPagerFragment newInstance() {
@@ -23,9 +31,14 @@ public class SongListViewPagerFragment extends BaseViewPagerFragment<LayoutSongl
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         mSongListAdapter = new SongListAdapter((OnClickSomething) getActivity());
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext(),LinearLayoutManager.VERTICAL,false);
         mBinding.recyclerviewSonglistfragment.setLayoutManager(layoutManager);
@@ -47,5 +60,24 @@ public class SongListViewPagerFragment extends BaseViewPagerFragment<LayoutSongl
     @Override
     public void queryBySearchView(String query) {
         mSongListAdapter.getFilter().filter(query);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_ID_REFRESH, MENU_ORDER_NUMBER, R.string.refresh)
+                .setIcon(android.R.drawable.ic_menu_rotate)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_ID_REFRESH:
+                mSongListAdapter.setDataSource(MediaUtils.getSongList(MyApplication.getContext()));
+                mSongListAdapter.notifyDataSetChanged();
+                return true;
+        }
+
+        return false;
     }
 }
