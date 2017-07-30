45ded978d1ca15c6bb59415c7a1b6c3d1fc5536f
tonynowater87 <tonynowater87@gmail.com>
2017-07-30 17:17:25 +0800

    搜尋List UI微調

--- smallplayer/app/src/main/java/com/tonynowater/smallplayer/view/CustomSearchAdapter.java
+++ smallplayer/app/src/main/java/com/tonynowater/smallplayer/view/CustomSearchAdapter.java
@@ -9,8 +9,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.tonynowater.smallplayer.R;
 import com.tonynowater.smallplayer.databinding.LayoutU2bsuggestionAdapterListItemBinding;
 
@@ -18,7 +20,7 @@ import com.tonynowater.smallplayer.databinding.LayoutU2bsuggestionAdapterListIte
  * Created by tonynowater on 2017/6/6.
  */
 
-public class CustomSearchAdapter extends CursorAdapter {
+public class  CustomSearchAdapter extends CursorAdapter {
     private static final String TAG = CustomSearchAdapter.class.getSimpleName();
     public static final String COLUMN_SUGGESTION = "suggestion";
     private LayoutU2bsuggestionAdapterListItemBinding mBinding;
@@ -35,6 +37,7 @@ public class CustomSearchAdapter extends CursorAdapter {
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         mBinding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.layout_u2bsuggestion_adapter_list_item, parent, false);
         ViewHolder viewHolder = new ViewHolder();
+        viewHolder.ivIcon = mBinding.ivLayoutU2bsuggestionAdapterListIcon;
         viewHolder.tvLeft = mBinding.tvLayoutU2bsuggestionAdapterListItem;
         viewHolder.tvRight = mBinding.tvRightLayoutU2bsuggestionAdapterListItem;
         mBinding.getRoot().setTag(viewHolder);
@@ -53,8 +56,13 @@ public class CustomSearchAdapter extends CursorAdapter {
 
         String suggestion = cursor.getString(cursor.getColumnIndex(COLUMN_SUGGESTION));
         if (TextUtils.equals(context.getString(R.string.search_bar_hint), suggestion)) {
+            viewHolder.tvLeft.setTextAppearance(context, android.R.style.TextAppearance_Material_Small);
+            viewHolder.ivIcon.setVisibility(View.GONE);
             viewHolder.tvRight.setVisibility(View.VISIBLE);
         } else {
+            viewHolder.tvLeft.setTextAppearance(context, android.R.style.TextAppearance_Material_Medium);
+            viewHolder.ivIcon.setVisibility(View.VISIBLE);
+            Glide.with(context).load(android.R.drawable.ic_menu_search).into(viewHolder.ivIcon);
             viewHolder.tvRight.setVisibility(View.GONE);
         }
 
@@ -62,6 +70,7 @@ public class CustomSearchAdapter extends CursorAdapter {
     }
 
     public static class ViewHolder {
+        ImageView ivIcon;
         TextView tvLeft;
         TextView tvRight;
     }
